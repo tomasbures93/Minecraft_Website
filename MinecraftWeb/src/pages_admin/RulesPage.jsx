e72e38f5a65477af36bfe3171abc1a95d6ffd1a8
tomasbures93 <98662129+tomasbures93@users.noreply.github.com>
2025-07-30 22:06:37 +0200

    Worked on RulesPage.jsx and ReactMarkdown

--- MinecraftWeb/src/pages_admin/RulesPage.jsx
+++ MinecraftWeb/src/pages_admin/RulesPage.jsx
@@ -1,13 +1,14 @@
-import { SpinnerGap, TextBolder, TextHThree, TextItalic, TextUnderline, CheckCircle } from "phosphor-react"
+import { SpinnerGap, TextBolder, TextHThree, TextItalic, CheckCircle, TextStrikethrough } from "phosphor-react"
 import NavbarAdmin from "../components_admin/NavbarAdmin";
 import ErrorAdmin from "../components_admin/ErrorAdmin";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 const RulesPage = () => {
     const [formData, setFormData] = useState([]);
     const [update, setUpdate] = useState(false);
     const [finish, setFinish] = useState(false);
     const [error, setError] = useState(false);
+    const textareaRef = useRef(null);
 
     useEffect(() => {
         fetch('https://localhost:7198/api/Website/GetRulesPage')
@@ -22,12 +23,7 @@ const RulesPage = () => {
     }
     ,[]);
 
-    const handleChange = (e) => {
-        const {name, value} = e.target;
-        setFormData({...formData, [name]: value});
-    }
-
-    const handleSubmit = async (e) => {
+        const handleSubmit = async (e) => {
         e.preventDefault();
         setUpdate(true);
         console.log(formData);
@@ -55,24 +51,76 @@ const RulesPage = () => {
         setUpdate(false);
     }
 
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormData({...formData, [name]: value});
+    }
+
+    const handleBold = () => {
+        const textarea = textareaRef.current;
+
+        const start = textarea.selectionStart;  // start
+        const end = textarea.selectionEnd;      // end
+
+        const text = textarea.value.substring(start, end);
+
+        const before = textarea.value.substring(0, start);
+        const after = textarea.value.substring(end);
+
+        const insertText = before + "**" + text + "**" + after;
+
+        setFormData({text: insertText});
+    }
+
+    const handleItalic = () => {
+        const textarea = textareaRef.current;
+
+        const start = textarea.selectionStart;
+        const end = textarea.selectionEnd;
+
+        const text = textarea.value.substring(start, end);
+
+        const before = textarea.value.substring(0, start);
+        const after = textarea.value.substring(end);
+
+        const insertText = before + "*" + text + "*" + after;
+        
+        setFormData({text: insertText});
+    }
+
+    const handleH3 = () => {
+        const textarea = textareaRef.current;
+
+        const start = textarea.selectionStart;
+        const end = textarea.selectionEnd;
+
+        const text = textarea.value.substring(start, end);
+
+        const before = textarea.value.substring(0, start);
+        const after = textarea.value.substring(end);
+
+        const insertText = before + "##" + text + after;
+
+        setFormData({text: insertText});
+    }
+
     return (
         <div className="p-4 mt-3 card-default danger-card">
             <NavbarAdmin />
             <h2>Rules Dashboard</h2>
             <form onSubmit={handleSubmit}>
                 <p>
-                    <TextBolder size={20} /> 
-                    <TextItalic size={20} />
-                    <TextUnderline size={20} />
-                    <TextHThree size={20} />
+                    <button onClick={handleBold} className="btn text-white border me-1 mb-1"><TextBolder size={20}/></button>
+                    <button onClick={handleItalic} className="btn text-white border me-1 mb-1"><TextItalic size={20} className="markDown"/></button>
+                    <button onClick={handleH3} className="btn text-white border me-1 mb-1"><TextHThree size={20} className="markDown" /></button>
+                    <textarea ref={textareaRef} className="form-control dark-input shadow" name="text" rows="15" onChange={handleChange} value={formData.text}></textarea>
+                    { update ?
+                        <button type="submit" className="btn btn-warning mt-3 shadow"><SpinnerGap size={20} className="spin"/>  Updating ...</button> :
+                        <button className="mt-3 btn btn-primary shadow">Update</button>
+                    }
+                    {error && <ErrorAdmin /> }
+                    {finish && <div className='mt-3 text-center mc-color'><CheckCircle size={20} /> Rules Updated</div>}
                 </p>
-                <textarea className="form-control dark-input shadow" name="text" rows="15" onChange={handleChange} value={formData.text}></textarea>
-                { update ?
-                    <button type="submit" className="btn btn-warning mt-3 shadow"><SpinnerGap size={20} className="spin"/>  Updating ...</button> :
-                    <button className="mt-3 btn btn-primary shadow">Update</button>
-                }
-                {error && <ErrorAdmin /> }
-                {finish && <div className='mt-3 text-center mc-color'><CheckCircle size={20} /> Rules Updated</div>}
             </form>
         </div>
     )
